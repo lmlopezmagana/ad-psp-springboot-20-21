6d5be262f587b99a745efa8afc0316c2ab12168a
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2020-09-24 10:32:51 +0200

    Primer ejemplo terminado

--- 01_EjemploWeb/src/main/java/com/salesianostriana/dam/ejemploweb/controllers/WelcomeController.java
+++ 01_EjemploWeb/src/main/java/com/salesianostriana/dam/ejemploweb/controllers/WelcomeController.java
@@ -4,14 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDateTime;
+
 @Controller
 public class WelcomeController {
 
-    @GetMapping()
+    @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("mensaje", "Los alumnos de 2º DAM no se acuerdan del curso pasado");
         return "index";
     }
 
+
+    @GetMapping("/date")
+    public String dameLaHora(Model model) {
+        model.addAttribute("fecha", LocalDateTime.now());
+        return "fecha";
+    }
 
 }
